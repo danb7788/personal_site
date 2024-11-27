cba378d189005da5538ae4e236330e10c0a4b60f
danberges <danberges@gmail.com>
2024-11-27 11:14:01 +0100

    Fixes

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -72,12 +72,12 @@
         <section id="{{ Str::slug(__('Bio')) }}">
             <h2>{{ __('Bio') }}</h2>
             @if (app()->getLocale() === 'es')
-                <p>Soy un profesor, autor y empresario español nacido en Madrid con <i>background</i> en arte, educación, lingüística y programación.
+                <p>Soy un profesor, autor y empresario español nacido en Madrid con <i>background</i> en arte, lingüística y programación.
                     Publico contenidos en Instagram, YouTube y TikTok sobre gramática, semántica y pragmática.</p>
                 <p>Fundé <a target="_blank" href="https://www.bergesinstitutespanish.com/">Berges Institute</a>
                     en 2013 en Nueva York, y después de vivir en Estados Unidos durante 15 años me volví a Madrid, donde vivo actualmente.</p>
             @else
-                <p>I’m a Madrid-born Spanish educator, author and entrepreneur with a background in arts, education, linguistics and computer programming.
+                <p>I’m a Madrid-born Spanish educator, author and entrepreneur with a background in arts, linguistics and computer programming.
                     I publish content on Instagram, YouTube and TikTok about grammar, semantics, and pragmatics.</p>
                 <p>I founded <a target="_blank" href="https://www.bergesinstitutespanish.com/">Berges Institute</a>
                     in 2013 in New York City, and after living in the United States for 15 years moved
